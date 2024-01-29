232954610a8c42a4ff56f387bf5057bd4372b81d
Peter Stromquist <peter@twistedstream.com>
2024-01-28 18:50:36 -0600

    refactor: upgrade pino to v8.17
    also:
    - replacing express-pino-logger (deprecated) with pino-http, fixing broken tests

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Express } from "express";
 import { engine } from "express-handlebars";
-import expressPino from "express-pino-logger";
+import pinoHttp from "pino-http";
 
 import errorHandler from "./error-handler";
 import { packageDescription, packageVersion } from "./utils/config";
@@ -10,7 +10,7 @@ import website from "./website";
 const app: Express = express();
 
 // App-level middleware
-app.use(expressPino({ logger }));
+app.use(pinoHttp({ logger }));
 
 app.use(express.static("public"));
 
